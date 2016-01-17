f85ae3f468c4da9b11ac4f62aedc8c3d8196a5ff
Loic Rowe <rowe.loic@gmail.com>
2016-01-17 19:12:27 +0100

    remove test_model, since it is divide in two files now, maybe we need more :)

--- tests/core_tests/test_controller.py
+++ tests/core_tests/test_controller.py
@@ -1,54 +1,63 @@
-import pytest
 from mongoengine import ValidationError as MongoValidationError
 
 from core.model_util import (Document, HistorizedDocument, BaseController,
                              ControlledDocument, VersionedDocument, fields)
 from core.concurrency import ConcurrencyError
+from core.core_app import CoreApp
 
 
-class TestController():
+class ControllerTestApp:
 
-    def test_controller(self):
-        def controller_factory(document):
-            router = {
-                'ready': ReadyController,
-                'fired': FiredController,
-                'reloading': ReloadingController
-            }
-            return router[document.status](document)
+    def __init__(self):
+        self.app = CoreApp("TestController")
+        self.app.bootstrap()
 
-        class ReadyController(BaseController):
+    def get(self):
+        return self.app
 
-            def state(self):
-                return "%s is ready to fire" % self.document.name
 
-            def fire(self):
-                self.document.status = 'fired'
-                self.document.save()
+def test_controller():
+    def controller_factory(document):
+        router = {
+            'ready': ReadyController,
+            'fired': FiredController,
+            'reloading': ReloadingController
+        }
+        return router[document.status](document)
 
-        class FiredController(BaseController):
+    class ReadyController(BaseController):
 
-            def state(self):
-                return "%s is empty" % self.document.name
+        def state(self):
+            return "%s is ready to fire" % self.document.name
 
-            def reload(self):
-                self.document.status = 'reloading'
-                self.document.save()
+        def fire(self):
+            self.document.status = 'fired'
+            self.document.save()
 
-        class ReloadingController(BaseController):
+    class FiredController(BaseController):
 
-            def state(self):
-                return "%s is reloading..." % self.document.name
+        def state(self):
+            return "%s is empty" % self.document.name
 
-            def done(self):
-                self.document.status = 'ready'
-                self.document.save()
+        def reload(self):
+            self.document.status = 'reloading'
+            self.document.save()
 
-        class Gun(ControlledDocument):
-            meta = {'controller_cls': controller_factory}
-            name = fields.StringField(required=True)
-            status = fields.StringField(choices=['ready', 'fired', 'reloading'], required=True)
+    class ReloadingController(BaseController):
 
+        def state(self):
+            return "%s is reloading..." % self.document.name
+
+        def done(self):
+            self.document.status = 'ready'
+            self.document.save()
+
+    class Gun(ControlledDocument):
+        meta = {'controller_cls': controller_factory}
+        name = fields.StringField(required=True)
+        status = fields.StringField(choices=['ready', 'fired', 'reloading'], required=True)
+    app = ControllerTestApp().get()
+    with app.app_context():
         doc = Gun(name='gun-1', status='ready')
         doc.save()
         assert isinstance(doc.controller, ReadyController)
@@ -61,17 +70,19 @@ class TestController():
         assert isinstance(doc.controller, ReloadingController)
         doc.controller.done()
 
-    def test_dualinheritance(self):
-        class DualInheritanceController(BaseController):
 
-            def make_v2(self):
-                self.document.field = 'v2'
-                self.document.save()
+def test_dualinheritance():
+    class DualInheritanceController(BaseController):
 
-        class DualInheritanceDoc(ControlledDocument, HistorizedDocument):
-            meta = {'controller_cls': DualInheritanceController}
-            field = fields.StringField()
+        def make_v2(self):
+            self.document.field = 'v2'
+            self.document.save()
 
+    class DualInheritanceDoc(ControlledDocument, HistorizedDocument):
+        meta = {'controller_cls': DualInheritanceController}
+        field = fields.StringField()
+    app = ControllerTestApp().get()
+    with app.app_context():
         doc = DualInheritanceDoc(field='v1')
         doc.save()
         # Make sure we have both history and controller functionalities
