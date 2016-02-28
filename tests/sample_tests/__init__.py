4074b57542303de5fcf55f526f76439e5956d779
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-02-28 23:32:56 +0100

    Remove useless tests

--- tests/sample_tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# Import the fixtures before importing any test to avoid import clash
-from sample_tests import fixtures
--- tests/sample_tests/fixtures.py
+++
@@ -1,8 +0,0 @@
-for cmd in [
-    "from tests.test_auth import user",
-    "from tests.test_user import another_user",
-]:
-    try:
-        exec(cmd)
-    except ImportError as e:
-        continue
