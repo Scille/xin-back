7982fea1f54ad4a0ac26f1d271a856b31176f14f
Loic Rowe <rowe.loic@gmail.com>
2016-04-04 10:24:19 +0200

    correction of the export name o the different function

--- xin/components/kalendar/app.py
+++ xin/components/kalendar/app.py
@@ -14,44 +14,44 @@ def treat_data(data):
     return 200, data.dump()
 
 
-@autobahn_sync.register('calendar.create')
+@autobahn_sync.register('kalendar.create')
 def calendar_create(name, users):
     calendar = create_calendar(name, users)
     return treat_data(calendar)
 
 
-@autobahn_sync.register('calendar.list')
+@autobahn_sync.register('kalendar.list')
 def calendar_list(page=1, per_page=20):
     cursor = list_calendar(page, per_page)
     calendars = [c.dump() for c in cursor]
     return 200, calendars
 
 
-@autobahn_sync.register('calendar.get')
+@autobahn_sync.register('kalendar.get')
 def calendar_get(calendar_id):
     calendar = get_calendar(ObjectId(calendar_id))
     return treat_data(calendar)
 
 
-@autobahn_sync.register('calendar.modify')
+@autobahn_sync.register('kalendar.modify')
 def calendar_modify(calendar_id, name=None, users=None, append=False):
     calendar = modify_calendar(ObjectId(calendar_id), name, users, append)
     return treat_data(calendar)
 
 
-@autobahn_sync.register('calendar.add_user')
+@autobahn_sync.register('kalendar.add_user')
 def calendar_add_user(calendar_id, user):
     calendar = add_user(ObjectId(calendar_id), user)
     return treat_data(calendar)
 
 
-@autobahn_sync.register('calendar.remove_user')
+@autobahn_sync.register('kalendar.remove_user')
 def calendar_remove_user(calendar_id, user):
     calendar = remove_user(ObjectId(calendar_id), user)
     return treat_data(calendar)
 
 
-@autobahn_sync.register('calendar.meeting.create')
+@autobahn_sync.register('kalendar.meeting.create')
 def meeting_create(calendar_id, name, users, date_begin, duration=None,
                    place=None, comment=None, tags=None):
     meeting = create_meeting(ObjectId(calendar_id), name, users, date_begin,
@@ -59,18 +59,18 @@ def meeting_create(calendar_id, name, users, date_begin, duration=None,
     return treat_data(meeting)
 
 
-@autobahn_sync.register('calendar.meeting.list')
+@autobahn_sync.register('kalendar.meeting.list')
 def meeting_list(calendar_id):
     return list_meetings(calendar_id)
 
 
-@autobahn_sync.register('calendar.meeting.get')
+@autobahn_sync.register('kalendar.meeting.get')
 def meeting_get(meeting_id):
     meeting = get_meeting(ObjectId(meeting_id))
     return treat_data(meeting)
 
 
-@autobahn_sync.register('calendar.meeting.modify')
+@autobahn_sync.register('kalendar.meeting.modify')
 def meeting_modify(meeting_id, name=None, users=None, append=False, date_begin=None,
                    duration=None, place=None, comment=None, tags=None):
     meeting = modify_meeting(meeting_id, name, users, append, date_begin,
@@ -78,7 +78,7 @@ def meeting_modify(meeting_id, name=None, users=None, append=False, date_begin=N
     return treat_data(meeting)
 
 
-@autobahn_sync.register('calendar.meeting.delete')
+@autobahn_sync.register('kalendar.meeting.delete')
 def meeting_delete(meeting_id):
     if delete_meeting(meeting_id):
         return 204
@@ -88,7 +88,7 @@ def meeting_delete(meeting_id):
 @autobahn_sync.on_challenge
 def on_challenge(challenge):
     import json
-    return json.dumps({'application_name': 'calendar', 'version': 0.1})
+    return json.dumps({'application_name': 'kalendar', 'version': 0.1})
 
 
 def main():
